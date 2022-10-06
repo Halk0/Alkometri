7288460a82855035820b7631106b4b12429d6183
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-10-06 19:33:47 +0300

    Moved styles to another file

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, ScrollView, View, TextInput, Button, Image } from 'react-native';
+import { Text, ScrollView, View, TextInput, Button, Image } from 'react-native';
 import { RadioButton } from './custom-components/radiobutton';
 import DropDownPicker from 'react-native-dropdown-picker';
+import { styles } from './styles'
 
 export default function App() {
   const [openBottle, setOpen] = useState(false);
@@ -85,45 +85,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    marginVertical: 100,
-    marginHorizontal: 20,
-    backgroundColor: '#fff',
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
-  imageView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  numberInput: {
-    width: '100%',
-    paddingBottom: 20,
-  },
-  radioButton: {
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    paddingBottom: 20
-  },
-  dropDown: {
-    paddingBottom: 20
-  },
-  submitButton: {
-    justifyContent: 'flex-start',
-    width: 49
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: '500',
-    paddingBottom: 10
-  },
-  result: {
-    fontSize: 20,
-    fontWeight: '500',
-    paddingTop: 50,
-    justifyContent: 'center'
-  }
-});
+
